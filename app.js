6b40cd740859b11370f346cee546c31aee2d91b7
EtihadStadium <mancity@mailbox.org>
2023-03-19 18:51:18 +0900

    Load username

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const loginForm = document.querySelector("form");
 const loginInput = document.querySelector("input");
 const profileMessage = document.querySelector("h1");
 const HIDDEN_CLASS_NAME = "hidden";
-loginForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const username = loginInput.value;
-    localStorage.setItem("username", username);
-    loginForm.classList.add(HIDDEN_CLASS_NAME);
+const USERNAME_KEY = "username";
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+function paintProfileMessage(username) {
     profileMessage.innerText = `Hello ${username}!`;
     profileMessage.classList.remove(HIDDEN_CLASS_NAME);
-});
+}
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASS_NAME);
+    loginForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const username = loginInput.value;
+        localStorage.setItem(USERNAME_KEY, username);
+        loginForm.classList.add(HIDDEN_CLASS_NAME);
+        paintProfileMessage(username);
+    });
+} else {
+    paintProfileMessage(savedUsername);
+}
