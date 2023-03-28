150ae90218a7b44a07a3c2c55f7d0d74f7f6d575
EtihadStadium <mancity@mailbox.org>
2023-03-28 17:03:36 +0900

    Modify temperature units

--- js/weather.js
+++ js/weather.js
@@ -4,12 +4,13 @@ const API_KEY = config.key;
 function onGeoOk(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
-            weather.innerText = `${data.weather[0].main}, ${data.main.temp} - ${data.name}`;
+            weather.innerText = `${data.weather[0].main}, ${data.main.temp.toFixed(1)}° - ${data.name}`;
         });
+    console.log(API_KEY);
 }
 function onGeoError() {
     alert("No weather information!");
