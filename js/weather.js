e95e7816c8adf5415ffc14d9f69d61bed9e0a37b
EtihadStadium <mancity@mailbox.org>
2023-03-28 15:33:08 +0900

    Add weather api

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,15 @@
+import { config } from "./apikey.js";
+const weather = document.getElementById("weather");
+const API_KEY = config.key;
 function onGeoOk(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    console.log(latitude, longitude);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            weather.innerText = `${data.weather[0].main}, ${data.main.temp} - ${data.name}`;
+        });
 }
 function onGeoError() {
     alert("No weather information!");
