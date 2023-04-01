c91ecccf1b18c2b6cf2a67cf0376f8e4bc152378
EtihadStadium <mancity@mailbox.org>
2023-04-02 00:00:27 +0900

    Add unsplash API background image feature
    Add JavaScript code to fetch a random image from the Unsplash API and set it as the page background.
    Also display image alt description and author name in a <span> element.

--- js/background.js
+++ js/background.js
@@ -1,6 +1,13 @@
 import { config } from "./apikey.js";
+const information = document.getElementById("information").querySelector("span");
+const body = document.querySelector("body");
 const API_KEY = config.unsplashKey;
-fetch(`https://api.unsplash.com/photos/random/?client_id=${API_KEY}`)
+const UNSPLASH_URL = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}&query=landscape&orientation=landscape`;
+fetch(UNSPLASH_URL)
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+        const imageUrl = data.urls.full;
+        body.style.backgroundImage = `url(${imageUrl})`;
+        information.innerText = `${String(data.alt_description)}, by ${String(data.user.name)}`;
+    })
     .catch((error) => console.error(error));
